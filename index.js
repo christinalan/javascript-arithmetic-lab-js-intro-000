0240ac66c268eef41af1e2e27ba0884771015112
Christina Lan <christina.lan13@gmail.com>
2018-10-10 17:17:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,9 @@ function multiply(a,b) {
 function divide(a,b) {
   return (a/b)
 }
-function inc(a) {
-  return (a+1)
+function inc(n) {
+  return n += 1;
 }
-function dec(a) {
-  return (a-1)
-}
-function makeInt(a, 'toString') {
-  return (a)
+function dec(n) {
+  return n -= 1;
 }
